6333a3afbe89e2c6999d6a77def7e0b09fd50cdd
Pierre Korsowski <pkorsowski@goodgamestudios.com>
2016-03-19 19:23:07 +0100

    reordered and comment phases of code generation added association of results of all steps, so you have access to codeDOM type, xsd definition and xml mapping structure for a specific type definition started to prepare custom type processors and with this also ScriptableObject will be created

--- Assets/Editor/XSDConverter.cs
+++ Assets/Editor/XSDConverter.cs
@@ -8,94 +8,208 @@ using System.CodeDom.Compiler;
 using Microsoft.CSharp;
 using System.Collections.Generic;
 using System.Text;
+using System;
+using System.Linq;
+using UnityEngine.Assertions;
 
-public class XSDConverter {
+namespace Xsd2So
+{
 
-    [MenuItem("XSD/Convert")]
-	public static void Convert()
+    public class DataRepresentation
     {
-        var path = Application.dataPath + "/XSD/shared.xsd";
-        StringBuilder console = new StringBuilder();
+        public XmlTypeMapping TypeMapping { get; set; }
+        public CodeTypeDeclaration CodeType { get; set; }
 
-        var content = File.ReadAllText(path);
-
-        XmlSchema xsd;
-        using (var stream = new StringReader(content))
-        {
-            xsd = XmlSchema.Read(stream, null);
-        }
-
-        console.AppendLine("xsd.IsCompiled = " + xsd.IsCompiled + "\n" + xsd.LineNumber + ", " + xsd.Version);
-
-        XmlSchemas xsds = new XmlSchemas();
-        xsds.Add(xsd);
-        xsds.Compile(null, true);
-        XmlSchemaImporter schemaImporter = new XmlSchemaImporter(xsds);
-
-        // create the codedom
-        CodeNamespace codeNamespace = new CodeNamespace("Generated");
-        XmlCodeExporter codeExporter = new XmlCodeExporter(codeNamespace);
-
-        List<XmlTypeMapping> maps = new List<XmlTypeMapping>();
-        foreach (XmlSchemaType schemaType in xsd.SchemaTypes.Values)
-        {
-            maps.Add(schemaImporter.ImportSchemaType(schemaType.QualifiedName));
-        }
-        foreach (XmlSchemaElement schemaElement in xsd.Elements.Values)
-        {
-            maps.Add(schemaImporter.ImportTypeMapping(schemaElement.QualifiedName));
-        }
-        foreach (XmlTypeMapping map in maps)
-        {
-            codeExporter.ExportTypeMapping(map);
-        }
-
-        console.AppendLine().AppendLine("#########################################").AppendLine();
-
-        RemoveAttributes(codeNamespace, console);
-
-        console.AppendLine().AppendLine("#########################################").AppendLine();
-
-        // Check for invalid characters in identifiers
-        //CodeGenerator.ValidateIdentifiers(codeNamespace);
-
-        // output the C# code
-        CSharpCodeProvider codeProvider = new CSharpCodeProvider();
-
-        using (StringWriter writer = new StringWriter())
-        {
-            codeProvider.GenerateCodeFromNamespace(codeNamespace, writer, new CodeGeneratorOptions());
-            console.AppendLine(writer.GetStringBuilder().ToString());
-        }
-
-        var filePath = Path.Combine(Application.dataPath, Path.Combine("XSD", "out.txt"));
-        File.WriteAllText(filePath, console.ToString());
-
-        AssetDatabase.Refresh();
+        public bool IsArray { get; internal set; }
     }
 
-    // Remove all the attributes from each type in the CodeNamespace, except
-    // System.Xml.Serialization.XmlTypeAttribute
-    private static void RemoveAttributes(CodeNamespace codeNamespace, StringBuilder console)
+    public class DataTypeRepresentation : DataRepresentation
     {
-        foreach (CodeTypeDeclaration codeType in codeNamespace.Types)
-        {
-            console.AppendLine(codeType.Name);
+        public XmlSchemaType XsdType { get; set; }
+        public bool IsComplexXsdType { get { return XsdType is XmlSchemaComplexType; } }
+    }
 
-            CodeAttributeDeclaration xmlTypeAttribute = null;
-            foreach (CodeAttributeDeclaration codeAttribute in codeType.CustomAttributes)
+    public class DataElementRepresentation : DataRepresentation
+    {
+        public XmlSchemaElement XsdType { get; set; }
+    }
+
+    class ModifiableTypeDuplicateComparer : IEqualityComparer<DataRepresentation>
+    {
+        public bool Equals(DataRepresentation x, DataRepresentation y)
+        {
+            if( x.CodeType == y.CodeType)
             {
-                if (codeAttribute.Name == "System.Xml.Serialization.XmlTypeAttribute")
+                Debug.Log("ModifiableTypeDuplicateComparer: DUPLICATE");
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(DataRepresentation obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    public class XSDConverter
+    {
+        /*
+            Need mapping XmlSchemaType to generated CodeDomType
+            Because CodeDomType contains getters/setters for attributes and also their
+                isSet state as attributes, if its an non-required XML attribute (most cases in GGS).
+
+            But XmlSchemaType can be XmlSchemaComplexType or XmlSchemaSimpleType
+                XmlSchemaSimpleType is converted to Enums
+                XmlSchemaComplexType is the "real" type definition and contains attributes
+
+            One can get the attributes list from XmlSchemaComplexType and from this get the real
+            attributes from the CodeDomeType and so differentiate it from the isSet attributes.
+
+            The isSet attribute is named "[xml attribute name]SpecifiedField", maybe this is the
+            cheap solution to differentiate it from normal attributes.
+
+            Only the non-isSet attributes from the CodeDomType have the real names, the private
+            fields have some numbers appended.
+        */
+
+        [MenuItem("XSD/Convert")]
+        public static void Convert()
+        {
+            // Load XSD as text file
+            var path = Application.dataPath + "/XSD/shared.xsd";
+            var content = File.ReadAllText(path);
+
+            // Parse Xsd to schema code representation
+            XmlSchema xsd;
+            using (var stream = new StringReader(content))
+            {
+                xsd = XmlSchema.Read(stream, null);
+            }
+
+            // Compile Xsd
+            XmlSchemas xsds = new XmlSchemas();
+            xsds.Add(xsd);
+            xsds.Compile(null, true);
+
+            // Generate type mapping for all Xsd types.
+            XmlSchemaImporter schemaImporter = new XmlSchemaImporter(xsds);
+            var typeList = new List<DataTypeRepresentation>(xsd.SchemaTypes.Values.Count);
+            foreach (XmlSchemaType schemaType in xsd.SchemaTypes.Values)
+            {
+                var type = schemaImporter.ImportSchemaType(schemaType.QualifiedName);
+                var typeRep = new DataTypeRepresentation();
+                typeRep.XsdType = schemaType;
+                typeRep.TypeMapping = type;
+
+                typeList.Add(typeRep);
+            }
+
+            // Generate type mapping for all Xsd elements.
+            var elementList = new List<DataElementRepresentation>(xsd.Elements.Values.Count);
+            foreach (XmlSchemaElement schemaElement in xsd.Elements.Values)
+            {
+                var type = schemaImporter.ImportTypeMapping(schemaElement.QualifiedName);
+                var typeRep = new DataElementRepresentation();
+                typeRep.XsdType = schemaElement;
+                typeRep.TypeMapping = type;
+
+                elementList.Add(typeRep);
+            }
+
+            // Export all xsd type mappings to the CodeDOM
+            CodeNamespace codeNamespace = new CodeNamespace("Generated");
+            XmlCodeExporter codeExporter = new XmlCodeExporter(codeNamespace);
+            foreach (var rep in typeList)
+            {
+                codeExporter.ExportTypeMapping(rep.TypeMapping);
+                AssociateXsdTypeWithCodeType(rep, codeNamespace);
+                if (rep.CodeType == null)
+                    Debug.LogError("Can't find code type for xsd type " + rep.TypeMapping.TypeName + " (element name '" + rep.TypeMapping.ElementName + "')");
+            }
+
+            foreach (var rep in elementList)
+            {
+                codeExporter.ExportTypeMapping(rep.TypeMapping);
+                AssociateXsdTypeWithCodeType(rep, codeNamespace);
+                if (rep.CodeType == null)
+                    Debug.LogError("Can't find code type for xsd type " + rep.TypeMapping.TypeName + "(element name '" + rep.TypeMapping.ElementName + "')");
+            }
+
+            // Create list of modifiable types, since some xsd types/elements might not have a code type representation (e.g. because they are arrays)
+            var modifiableTypes = new List<DataRepresentation>();
+            modifiableTypes.AddRange(typeList.FindAll(rep => rep.CodeType != null).Cast<DataRepresentation>());
+            modifiableTypes.AddRange(elementList.FindAll(rep => rep.CodeType != null).Cast<DataRepresentation>());
+
+            var userModifiableTypes = modifiableTypes.Distinct(new ModifiableTypeDuplicateComparer());
+
+            Assert.AreEqual(codeNamespace.Types.Count, userModifiableTypes.Count());
+
+            // Modify types
+            RemoveAttributes(codeNamespace);
+
+            // Check for invalid characters in identifiers
+            //CodeGenerator.ValidateIdentifiers(codeNamespace); // no implemented in Unity's Mono, has to be handwritten
+
+            // output the C# code
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+
+            StringBuilder code;
+            using (StringWriter writer = new StringWriter())
+            {
+                codeProvider.GenerateCodeFromNamespace(codeNamespace, writer, new CodeGeneratorOptions());
+                code = writer.GetStringBuilder();
+                Debug.Log(code.ToString());
+            }
+
+            var filePath = Path.Combine(Application.dataPath, Path.Combine("XSD", "out.txt"));
+            File.WriteAllText(filePath, code.ToString());
+
+            // Refresh Unity to compile everything
+            AssetDatabase.Refresh();
+        }
+
+        private static void AssociateXsdTypeWithCodeType(DataRepresentation rep, CodeNamespace codeNamespace)
+        {
+            var repXsd = rep.TypeMapping;
+            string xsdTypeName = repXsd.TypeName;
+            if (xsdTypeName.EndsWith("[]"))
+            {
+                rep.IsArray = true;
+                rep.CodeType = null;
+                return;
+            }
+
+            foreach (CodeTypeDeclaration type in codeNamespace.Types)
+            {
+                if (type.Name == xsdTypeName)
                 {
-                    xmlTypeAttribute = codeAttribute;
+                    rep.CodeType = type;
+                    return;
                 }
             }
-            codeType.CustomAttributes.Clear();
-            if (xmlTypeAttribute != null)
+        }
+
+        // Remove all the attributes from each type in the CodeNamespace, except
+        // System.Xml.Serialization.XmlTypeAttribute
+        private static void RemoveAttributes(CodeNamespace codeNamespace)
+        {
+            foreach (CodeTypeDeclaration codeType in codeNamespace.Types)
             {
-                codeType.CustomAttributes.Add(xmlTypeAttribute);
+                CodeAttributeDeclaration xmlTypeAttribute = null;
+                foreach (CodeAttributeDeclaration codeAttribute in codeType.CustomAttributes)
+                {
+                    if (codeAttribute.Name == "System.Xml.Serialization.XmlTypeAttribute")
+                    {
+                        xmlTypeAttribute = codeAttribute;
+                    }
+                }
+                codeType.CustomAttributes.Clear();
+                if (xmlTypeAttribute != null)
+                {
+                    codeType.CustomAttributes.Add(xmlTypeAttribute);
+                }
             }
         }
     }
-
 }
