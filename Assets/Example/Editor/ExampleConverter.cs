b17f42a447f423b32247295253d445a60d3cca23
Pierre Korsowski <myrtanim@myrtanim.de>
2016-07-19 22:48:47 +0200

    removed superflous meta files; made project compilable again by commenting code; added ignore file after moving to hg

--- Assets/Example/Editor/ExampleConverter.cs
+++ Assets/Example/Editor/ExampleConverter.cs
@@ -1,5 +1,5 @@
-﻿using Example.Generated;
-using Example.Generated.Editor;
+﻿//using Example.Generated;
+//using Example.Generated.Editor;
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
@@ -13,25 +13,25 @@ namespace Xsd2So.Assets.Example.Editor
 		[MenuItem("XSD/Example/Load and transfer data")]
 		public static void LoadAndConvert()
 		{
-			var xmlText = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Example/XSD/test.xml");
+			//var xmlText = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Example/XSD/test.xml");
 
-			using(var xmlReader = new XmlTextReader(new StringReader(xmlText.text)))
-			{
-				xmlReader.Namespaces = true;
+			//using(var xmlReader = new XmlTextReader(new StringReader(xmlText.text)))
+			//{
+			//	xmlReader.Namespaces = true;
 
-				XmlSerializer serializer = new XmlSerializer(typeof(BalancingData));
-				BalancingData xmlData = (BalancingData)serializer.Deserialize(xmlReader);
+			//	XmlSerializer serializer = new XmlSerializer(typeof(XsdClass));
+			//	XsdClass xmlData = (XsdClass)serializer.Deserialize(xmlReader);
 
-				var soInstance = ScriptableObject.CreateInstance<BalancingDataSO>();
+			//	var soInstance = ScriptableObject.CreateInstance<SOClass>();
 
-				xmlData.ToSerializable(soInstance);
+			//	xmlData.ToSerializable(soInstance);
 
-				Directory.CreateDirectory("Assets/Resources/XSD");
-				AssetDatabase.CreateAsset(soInstance, "Assets/Resources/XSD/testSO.asset");
-				AssetDatabase.SaveAssets();
-				EditorUtility.FocusProjectWindow();
-				Selection.activeObject = soInstance;
-			}
+			//	Directory.CreateDirectory("Assets/Resources/XSD");
+			//	AssetDatabase.CreateAsset(soInstance, "Assets/Resources/XSD/testSO.asset");
+			//	AssetDatabase.SaveAssets();
+			//	EditorUtility.FocusProjectWindow();
+			//	Selection.activeObject = soInstance;
+			//}
 		}
 	}
 }
